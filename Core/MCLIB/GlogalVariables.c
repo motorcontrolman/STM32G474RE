1207073e79bb5c1879dd681e3066f22110c33d64
motorcontrolman <matsuki.yosuke38@gmail.com>
2023-07-19 23:19:29 +0900

    first commit

---
+++ Core/MCLIB/GlogalVariables.c
@@ -0,0 +1,28 @@
+/*
+ * common.c
+ *
+ *  Created on: Apr 22, 2023
+ *      Author: r720r
+ */
+
+
+#include <stdint.h>
+#include "GlogalVariables.h"
+#include "main.h"
+
+uint16_t gAdcValue[2];
+uint8_t gHall[3];
+uint8_t gButton1;
+uint32_t gTIMCounter;
+uint32_t gTIMCounter_pre;
+uint32_t gInputCaptureCnt;
+uint32_t gInputCaptureCnt_pre;
+float gElectFreq = 0;
+float gTheta = 0;
+float gVdc;
+float gVolume;
+float gIuvw[3];
+uint16_t gIuvw_AD[3];
+int8_t gOutputMode[3];
+float gDutyRef = 0;
+float gDuty[3];
---
+++ Core/MCLIB/SixsStep.h
@@ -0,0 +1,17 @@
+/*
+ * Hall.h
+ *
+ *  Created on: Apr 22, 2023
+ *      Author: r720r
+ */
+
+#ifndef ORIGINAL_SIXSSTEP_H_
+#define ORIGINAL_SIXSSTEP_H_
+
+#include <stdint.h>
+#include "main.h"
+
+// Global Functions
+void sixStepTasks(float DutyRef, float leadAngle, float* Theta, float* Duty, float* outputMode);
+
+#endif /* ORIGINAL_SIXSSTEP_H_ */
